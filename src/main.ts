d1c366ae571a4538316fbb4c4fb805e216a8fb93
Jax <294242589@qq.com>
2023-03-29 15:44:16 +0800

    feat: swagger

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,12 @@ import { AppModule } from './app.module';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 import { AllExceptionsFilter } from './common/exceptions/base.exception.filter';
 import { HttpExceptionFilter } from './common/exceptions/http.exception.filter';
+import { generateDocument } from './doc';
+
 declare const module: any;
 
 async function bootstrap() {
-  if (module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => app.close());
-  }
-  
+
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
@@ -25,6 +23,16 @@ async function bootstrap() {
   // 异常过滤器
   app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter());
 
+  // 创建文档
+  generateDocument(app)
+
+  // 添加热更新
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+
   await app.listen(3000);
 }
+
 bootstrap();
